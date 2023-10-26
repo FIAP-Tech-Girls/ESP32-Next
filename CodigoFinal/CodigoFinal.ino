2aac217ed9da2e955437b423dd1ed5ec6c49823b
letyresina <leticiaresina@gmail.com>
2023-10-26 20:32:35 -0300

    Subindo definindo sensores, leds e botões

---
+++ CodigoFinal/CodigoFinal.ino
@@ -0,0 +1,29 @@
+// Sensor de distância 
+
+#include "Adafruit_VL53L0X.h"
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+// Definindo leds
+
+// Carros
+int ledVermelhoCarros = 33;
+int ledAmareloCarros = 25;
+int ledVerdeCarros = 26;
+
+// Pedestres
+int ledVermelhoPedestres = 27;
+int ledVerdePedestres = 32;
+
+// Definindo botão 
+int buttonPin = 18;
+
+void setup() {
+  
+
+}
+
+void loop() {
+  
+
+}
