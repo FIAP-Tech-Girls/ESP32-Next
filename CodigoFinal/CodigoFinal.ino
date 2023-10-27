59386f2ae19a351e8e230a0eae9eda0bbd837f47
letyresina <leticiaresina@gmail.com>
2023-10-26 22:32:08 -0300

    lógica do botão

--- CodigoFinal/CodigoFinal.ino
+++ CodigoFinal/CodigoFinal.ino
@@ -16,7 +16,7 @@ int ledVermelhoPedestres = 27;
 int ledVerdePedestres = 32;
 
 // Definindo botão 
-int buttonPin = 18;
+int botaoPedestres = 18;
 
 // Defina os estados para controlar o semáforo dos carros
 enum CarState {
@@ -34,7 +34,7 @@ void setup() {
   Serial.begin(115200);
   
   // Botão
-  pinMode(buttonPin,  INPUT_PULLUP);
+  pinMode(botaoPedestres,  INPUT_PULLUP);
 
   // Leds - Carros
   pinMode(luzVermelhaCarros, OUTPUT);
@@ -48,6 +48,52 @@ void setup() {
 
 void loop() {
   
+  VL53L0X_RangingMeasurementData_t measure;
+  
+  Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println(" out of range ");
+  }
+    
+  delay(100);
+
+  int distancia = measure.RangeMilliMeter;
+    
+  // Determinando o estado do semáforo com base na leitura da distância
+  if (distancia >= 200) {
+    carroContador = 0;
+  } else if (distancia < 200 && distancia >= 20) {
+    carroContador++;
+  }    
+
+  // Verificar se o botão de pedestres foi pressionado
+  if (digitalRead(botaoPedestres) == LOW) {
+    botaoPressionado = true;
+  }
+
+  // Lógica para o botão de pedestres pressionado
+  if (botaoPressionado) {
+    apagaLed(); // Desliga todas as luzes
+
+    digitalWrite(luzVermelhaCarros, HIGH); // Fecha o semáforo dos carros
+    digitalWrite(luzAmarelaCarros, HIGH);
+
+    delay(2000); // Delay de 2 segundos para a transição
+
+    digitalWrite(luzAmarelaCarros, LOW); // Desliga a luz amarela dos carros
+
+    digitalWrite(luzVerdePedestres, HIGH); // Abre o semáforo dos pedestres
+
+    delay(5000); // Tempo de abertura do semáforo dos pedestres (5 segundos neste exemplo)
+
+    digitalWrite(luzVerdePedestres, LOW); // Fecha o semáforo dos pedestres
+
+    botaoPressionado = false; // Reinicia a variável de botão pressionado para falso
+  }
 }
 
 // Para facilitar o código, apagando todos os leds e deixando apenas os necessários acesos!
